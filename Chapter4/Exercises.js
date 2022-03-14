bde03ed4a2e5a9a8b6ca24674bddab035c009924
minhhuy18it <78481059+minhhuy18it@users.noreply.github.com>
2022-03-14 21:51:41 +0700

    Update Exercises.js

--- Chapter4/Exercises.js
+++ Chapter4/Exercises.js
@@ -6,7 +6,6 @@ function range(start, end, step = start <= end ? 1 : -1) {
     }
     return result;
 }
-
 function sum(number) {
     result = 0;
     for (let num of number) {
@@ -19,3 +18,25 @@ console.log(sum(range(1, 10)));
 
 
 //Reversing an Array
+function reverseArray(array) {
+    result = [];
+    for (let item of array) {
+        result.unshift(item);
+    }
+    return result;
+}
+function reverseArrayInPlace(array) {
+    let len = array.length;
+    for (let i = 0; i < Math.floor(len/2); i++) {
+        console.log(i, len-i-1, array[i], array[len-i-1], array);
+        let swap = array[i];
+        array[i] = array[len-i-1];
+        array[len-i-1] = swap;
+    }
+    return array;
+}
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
