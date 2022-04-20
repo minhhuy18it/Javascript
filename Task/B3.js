311def25160657f4c630c576d192941974bc50dd
minhhuy18it <78481059+minhhuy18it@users.noreply.github.com>
2022-04-20 11:04:51 +0700

    Create B3.js

---
+++ Task/B3.js
@@ -0,0 +1,25 @@
+const controller = async (req, res) => {
+    doA()
+
+    await doB()
+    res.status(200).end()
+}
+
+const doA = () => {
+    return new Promise((resolve, reject) => {
+        resolve()
+        console.log('Display doA')
+    })
+
+}
+const doB = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+            console.log('Display doB')
+        }, 1000)
+    })
+}
+controller()
+/*Trong event loop có 3 giai đoạn ,call stack WebAPIs Queue, Call stack có vai trò lưu trữ 
+*/
